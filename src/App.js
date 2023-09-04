389156b58ee8dc69ac92a648492153e1ceee3e0f
vezy <vezymukti@gmail.com>
2023-09-04 11:49:56 +0700

    api undefined

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect } from 'react';
+import './App.css';
+import {getMovieList, searchMovie} from "./api"
+
+const App = () => {
+
+
+  useEffect (() => {
+      getMovieList()
+  }, [])
+  const search = (q) => {
+    console.log({q})
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>jeconia movie.</h1>
+        <input placeholder='Cari film favorit..' className='Movie-search'
+        onChange={(target) => search(target.value)}
+        
+        />
+        <div className="Movie-container">
+          
+          <div className="Movie-wrapper">
+            <div className="Movie-title">Contoh Film Pertama</div>
+              <img className="Movie-images" src=''/>
+                <div className="Movie-date">17-06-2023</div>
+                  <div className="Movie-rate">9,0</div>
+          </div>
+          
+        </div>
+        
       </header>
     </div>
   );
